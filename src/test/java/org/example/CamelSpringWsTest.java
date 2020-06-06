4236cb29c8f85cf970aa8b817389da74f87a6c98
Ricky Chiang <metavige@gmail.com>
2020-06-06 14:53:26 +0800

    feat: mark spring-ws test code
    還沒有找到做法，猜測是使用在 Java DSL 內的語法，跟 Spring 還沒辦法整合？

--- src/test/java/org/example/CamelSpringWsTest.java
+++ src/test/java/org/example/CamelSpringWsTest.java
@@ -1,19 +1,14 @@
 package org.example;
 
 import org.apache.camel.EndpointInject;
-import org.apache.camel.Exchange;
-import org.apache.camel.Processor;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.model.dataformat.SoapJaxbDataFormat;
 import org.apache.camel.test.junit4.CamelTestSupport;
-import org.apache.camel.test.spring.CamelSpringDelegatingTestContextLoader;
 import org.apache.camel.test.spring.CamelSpringRunner;
 import org.apache.camel.test.spring.CamelTestContextBootstrapper;
-import org.apache.camel.test.spring.DisableJmx;
-import org.example.soap.WsClient;
 import org.example.soap.WsClientConfig;
 import org.example.wsdl.Add;
 import org.example.wsdl.AddResponse;
@@ -21,22 +16,24 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.test.context.BootstrapWith;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.ws.client.core.WebServiceTemplate;
 
-//@RunWith(SpringJUnit4ClassRunner.class)
 @RunWith(CamelSpringRunner.class)
 @BootstrapWith(CamelTestContextBootstrapper.class)
 @ContextConfiguration(classes = {
-  WsClientConfig.class,
-  CamelSpringWsTest.class
-}, loader = CamelSpringDelegatingTestContextLoader.class)
+  WsClientConfig.class
+}, loader = AnnotationConfigContextLoader.class)
+@EnableAutoConfiguration
 public class CamelSpringWsTest extends CamelTestSupport {
 
   // region Fields
 
-  String contextPath = "org.example.wsdl";
+  private String contextPath = "org.example.wsdl";
+  private String webServiceUri = "http://www.dneonline.com/calculator.asmx";
   //  String schemaPath = "classpath:schemas/calculator.wsdl";
 
   @EndpointInject("mock:out")
@@ -51,40 +48,88 @@ public class CamelSpringWsTest extends CamelTestSupport {
 
   // region Methods
 
+  /**
+   * 測試使用 WebServiceClient 來直接呼叫 WebService，這邊使用的是 @see Processor
+   * @throws Exception 測試發生錯誤
+   */
   @Test
-  public void testCallSpringWs() throws Exception {
+  public void testSpringWsFromProcess() throws Exception {
 
     // 1. Assign
     context.addRoutes(new RouteBuilder() {
 
-      @Override public void configure() throws Exception {
-
-        SoapJaxbDataFormat jaxb = new SoapJaxbDataFormat(contextPath);
-        jaxb.setVersion("1.2");
+      @Override public void configure() {
 
         from("direct:in")
-          //          .marshal(jaxb)
           .log("${body}")
-          //          .to("spring-ws:http://www.dneonline.com/calculator.asmx")
           .process(exchange -> {
 
-            Object response = webServiceTemplate.marshalSendAndReceive(
-              "http://www.dneonline.com/calculator.asmx",
-              exchange.getIn().getBody());
+            Object response = webServiceTemplate
+              .marshalSendAndReceive(webServiceUri, exchange.getIn().getBody());
             exchange.getMessage().setBody(response);
 
           })
           .log("${body}")
-          //          .unmarshal(jaxb)
           .to("mock:out");
 
-        getContext().setTracing(true);
+//        getContext().setTracing(true);
       }
     });
 
     int a = 2, b = 7;
 
-    //    MockEndpoint resultEndpoint = getMockEndpoint("mock:out");
+    resultEndpoint.expectedMessageCount(1);
+    resultEndpoint.expectedMessagesMatches(m -> {
+      AddResponse addResponse = m.getIn().getBody(AddResponse.class);
+      Assert.assertNotNull(addResponse);
+      Assert.assertEquals(a + b, addResponse.getAddResult());
+
+      return true;
+    });
+
+    // 2. Act
+    Add addReq = new Add();
+    addReq.setIntA(a);
+    addReq.setIntB(b);
+
+    template.sendBody(addReq);
+
+    // 3. Assert
+    MockEndpoint.assertIsSatisfied(context);
+  }
+
+  /**
+   * 測試使用 Camel 提供的 Spring WS 支援來呼叫 WebService
+   *
+   * TODO: 這部分因為對 Inject 進去的 Bean 有問題，還沒測試出來
+   *
+   * @throws Exception 測試發生錯誤
+   */
+//  @Test
+  public void testCamelSpringWebService() throws Exception {
+
+    // 1. Assign
+    context.addRoutes(new RouteBuilder() {
+
+      @Override public void configure() {
+
+        SoapJaxbDataFormat jaxb = new SoapJaxbDataFormat(contextPath);
+        jaxb.setVersion("1.2");
+
+        from("direct:in")
+          .marshal(jaxb)
+          .log("${body}")
+          .to(String.format("spring-ws:%s?webServiceTemplate=#wst", webServiceUri))
+          .log("${body}")
+          .unmarshal(jaxb)
+          .to("mock:out");
+
+//        getContext().setTracing(true);
+      }
+    });
+
+    int a = 2, b = 7;
+
     resultEndpoint.expectedMessageCount(1);
     resultEndpoint.expectedMessagesMatches(m -> {
       AddResponse addResponse = m.getIn().getBody(AddResponse.class);
@@ -102,8 +147,7 @@ public class CamelSpringWsTest extends CamelTestSupport {
     template.sendBody("direct:in", addReq);
 
     // 3. Assert
-    assertMockEndpointsSatisfied();
+    MockEndpoint.assertIsSatisfied(context);
   }
-
   // endregion
 }
