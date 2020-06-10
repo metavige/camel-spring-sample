5b7eeb1a2d825ad05f735c6897132b86e7d8537c
metavige <metavige@gmail.com>
2020-06-10 21:27:19 +0800

    feat: 找到了怎麼注入測試案例中的 Camel Bean
    確認 Spring-WS 的使用方式，以及輸出輸入的格式
    所以也確認了怎麼注入 Bean 給 spring-ws 使用

--- src/test/java/org/example/CamelSpringWsTest.java
+++ src/test/java/org/example/CamelSpringWsTest.java
@@ -1,11 +1,14 @@
 package org.example;
 
 import org.apache.camel.EndpointInject;
+import org.apache.camel.Exchange;
+import org.apache.camel.Expression;
 import org.apache.camel.Produce;
 import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.mock.MockEndpoint;
 import org.apache.camel.model.dataformat.SoapJaxbDataFormat;
+import org.apache.camel.spi.Registry;
 import org.apache.camel.test.junit4.CamelTestSupport;
 import org.apache.camel.test.spring.CamelSpringRunner;
 import org.apache.camel.test.spring.CamelTestContextBootstrapper;
@@ -17,10 +20,14 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.test.context.BootstrapWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
+import org.springframework.ws.WebServiceMessageFactory;
 import org.springframework.ws.client.core.WebServiceTemplate;
+
+import javax.xml.transform.Source;
 
 @RunWith(CamelSpringRunner.class)
 @BootstrapWith(CamelTestContextBootstrapper.class)
@@ -32,9 +39,7 @@ public class CamelSpringWsTest extends CamelTestSupport {
 
   // region Fields
 
-  private String contextPath = "org.example.wsdl";
   private String webServiceUri = "http://www.dneonline.com/calculator.asmx";
-  //  String schemaPath = "classpath:schemas/calculator.wsdl";
 
   @EndpointInject("mock:out")
   protected MockEndpoint resultEndpoint;
@@ -44,12 +49,20 @@ public class CamelSpringWsTest extends CamelTestSupport {
 
   @Autowired
   private WebServiceTemplate webServiceTemplate;
+
+  @Autowired
+  private WebServiceMessageFactory messageFactory;
+
+  @Autowired
+  private Jaxb2Marshaller marshaller;
+
   // endregion
 
   // region Methods
 
   /**
    * 測試使用 WebServiceClient 來直接呼叫 WebService，這邊使用的是 @see Processor
+   *
    * @throws Exception 測試發生錯誤
    */
   @Test
@@ -64,6 +77,8 @@ public class CamelSpringWsTest extends CamelTestSupport {
           .log("${body}")
           .process(exchange -> {
 
+            // 直接用 Process, 呼叫 Spring-WS WebServiceTemplate 來處理 SOAP
+            // 這樣的確比較容易懂，而且相對來講，不太依賴 Camel Spring-WS Endpoint
             Object response = webServiceTemplate
               .marshalSendAndReceive(webServiceUri, exchange.getIn().getBody());
             exchange.getMessage().setBody(response);
@@ -72,7 +87,7 @@ public class CamelSpringWsTest extends CamelTestSupport {
           .log("${body}")
           .to("mock:out");
 
-//        getContext().setTracing(true);
+        //        getContext().setTracing(true);
       }
     });
 
@@ -101,11 +116,9 @@ public class CamelSpringWsTest extends CamelTestSupport {
   /**
    * 測試使用 Camel 提供的 Spring WS 支援來呼叫 WebService
    *
-   * TODO: 這部分因為對 Inject 進去的 Bean 有問題，還沒測試出來
-   *
    * @throws Exception 測試發生錯誤
    */
-//  @Test
+  @Test
   public void testCamelSpringWebService() throws Exception {
 
     // 1. Assign
@@ -113,18 +126,20 @@ public class CamelSpringWsTest extends CamelTestSupport {
 
       @Override public void configure() {
 
-        SoapJaxbDataFormat jaxb = new SoapJaxbDataFormat(contextPath);
-        jaxb.setVersion("1.2");
-
+        // 這邊只需要注入 messageFactory, 讓他支援 SOAP 1.2 就可以
+        // 測試案例啟動的時候，會去呼叫 bindToRegistry 方法註冊要用的 Bean
+        // 目前找到是要在這邊註冊一個要用的 Bean (之後再測試實際使用 Spring 正式執行是否不需要)
         from("direct:in")
-          .marshal(jaxb)
-          .log("${body}")
-          .to(String.format("spring-ws:%s?webServiceTemplate=#wst", webServiceUri))
-          .log("${body}")
-          .unmarshal(jaxb)
-          .to("mock:out");
+          .to(String.format("spring-ws:%s?messageFactory=#messageFactory", webServiceUri))
+          .transform(new Expression() {
 
-//        getContext().setTracing(true);
+            @Override public <T> T evaluate(Exchange exchange, Class<T> type) {
+              // 用來轉換 SOAP XML -> Java Bean
+              return (T) marshaller.unmarshal(exchange.getIn().getBody(Source.class));
+            }
+          })
+          .log("output: ${body}")
+          .to("mock:out");
       }
     });
 
@@ -149,5 +164,17 @@ public class CamelSpringWsTest extends CamelTestSupport {
     // 3. Assert
     MockEndpoint.assertIsSatisfied(context);
   }
+
+  /**
+   * 協助注入要提供給 Camel 內使用的 Bean
+   *
+   * @param registry
+   * @throws Exception
+   */
+  @Override protected void bindToRegistry(Registry registry) throws Exception {
+
+    registry.bind("messageFactory", messageFactory);
+  }
+
   // endregion
 }
